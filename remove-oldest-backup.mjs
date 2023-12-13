f597e920061c06475191f9b3d008bef2217d22ad
Jakub Gladykowski <gladykov@gmail.com>
2023-12-13 14:49:27 -0300

    All good here

---
+++ remove-oldest-backup.mjs
@@ -0,0 +1,50 @@
+import * as fs from 'fs';
+import path from 'path';
+
+console.log("I;m here")
+
+const backupPath = process.env.BACKUP_PATH;
+console.log(backupPath)
+
+const backupsToKeep = 3
+
+function getSortedFiles (dir) {
+    const files = fs.readdirSync(dir);
+
+    return files
+        .map(fileName => ({
+            name: fileName,
+            time: fs.statSync(`${dir}/${fileName}`).birthtime.getTime(),
+        }))
+        .sort((a, b) => b.time - a.time)
+        .map(file => file.name);
+};
+
+function removeBackups(files) {
+    while (files.length > backupsToKeep) {
+        const fileToRemove = files[files.length-1]
+        console.log("Will remove")
+        console.log(path.join(backupPath, fileToRemove))
+        // fs.unlinkSync(path.join(backupPath, fileToRemove))
+        files.pop()
+    }
+}
+
+const sortedFiles = getSortedFiles(backupPath)
+const confFiles = sortedFiles.filter(el => el.includes("conf"))
+const jiraFiles = sortedFiles.filter(el => el.includes("jira"))
+
+console.log(sortedFiles)
+console.log(confFiles)
+console.log(jiraFiles)
+
+if (confFiles.length > backupsToKeep) {
+    removeBackups(confFiles)
+}
+
+if (jiraFiles.length > backupsToKeep) {
+    removeBackups(jiraFiles)
+}
+
+
+
